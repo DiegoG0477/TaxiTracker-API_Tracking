40f57b53a2a5162536cbf8c41c4b9fbd8a033b97
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-07-20 13:28:15 -0600

    jwt sign microservice implemented

--- src/shared/services/AuthService.ts
+++ src/shared/services/AuthService.ts
@@ -1,8 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export class AuthService {
     async generateToken(secretKey: string): Promise<string> {
-        // Generate a token using the secret key
-        // 
-        // const token = https://aws.lambda/secret-key-generator-service
-        return secretKey;
+        const awsLambdaUrl = process.env.AWS_LAMBDA_JWT_URL ?? "";
+        const response = await fetch(awsLambdaUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ 
+                "secret_key": secretKey 
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error status handling jwt sign: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return data.token;
     }
 }
