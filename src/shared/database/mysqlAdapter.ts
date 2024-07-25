ff8b66025c1f490c4ba353c5fe25c86966b2ba98
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-07-25 15:36:09 -0600

    db port

--- src/shared/database/mysqlAdapter.ts
+++ src/shared/database/mysqlAdapter.ts
@@ -5,11 +5,14 @@ import { Signale } from "signale";
 dotenv.config();
 const signale = new Signale();
 
+const DB_PORT = parseInt(process.env.DB_PORT ?? '3306', 10);
+
 const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
+  port: DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
 };
