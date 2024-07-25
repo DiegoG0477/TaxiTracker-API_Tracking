c569445b53764cc72cf5c079e2dfffc092d33e15
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-07-25 16:09:45 -0600

    update

--- src/driving/infraestructure/adapters/MysqlDrivingRepository.ts
+++ src/driving/infraestructure/adapters/MysqlDrivingRepository.ts
@@ -1,13 +1,25 @@
 import { query } from "../../../shared/database/mysqlAdapter";
 import { Driving } from "../../domain/entities/Driving";
 import { DrivingRepository } from "../../domain/DrivingRepository";
-//         const sql = "CALL registerCrash(?, ?, ?, ?, ?)";
-//         const params: any[] = [crash.kit_id, crash.driver_id, crash.datetime, crash.impact_force, crash.crash_coordinates];
+
 export class MysqlDrivingRepository implements DrivingRepository {
     async registerDriving(driving: Driving): Promise<Driving | null> {
         let drivingResult: any = null;
         const queryStr: string = 'CALL registerDrivingData(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        const values: any[] = [driving.kit_id, driving.driver_id, driving.travel_id, driving.datetime, driving.acceleration, driving.deceleration, driving.vibrations, driving.travel_coordinates];
+        const values: any[] = [
+            driving.kit_id, 
+            driving.driver_id, 
+            driving.travel_id, 
+            driving.datetime, 
+            driving.travel_coordinates, 
+            driving.vibrations, 
+            driving.acceleration, 
+            driving.deceleration, 
+            driving.inclination_angle, 
+            driving.angular_velocity, 
+            driving.g_force_x, 
+            driving.g_force_y
+        ];
 
         try {
             const [result]: any = await query(queryStr, values);
